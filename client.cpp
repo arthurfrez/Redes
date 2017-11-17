b4d5a525002b1232efbf9b3463fb0c15d05b4e1d
Arthur F. Rezende <arthurfrezende@gmail.com>
2017-11-17 14:57:12 -0200

    Initial Commit

---
+++ client.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <winsock2.h>
+
+using namespace std;
+
+// -lws2_32
+
+int main() {
+    WSADATA WSAData;
+    SOCKET server;
+    SOCKADDR_IN addr;
+
+    WSAStartup(MAKEWORD(2,0), &WSAData);
+    server = socket(AF_INET, SOCK_STREAM, 0);
+
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // ip mesma maquina
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(5555);
+
+    connect(server, (SOCKADDR *)&addr, sizeof(addr));
+    printf("Connected to server!\n");
+
+
+    while(true) {
+      printf("Mensagem a ser enviada: ");
+      char buffer[1024];
+      fgets(buffer, 1024, stdin);
+
+      // removendo a quebra de linha
+      int pos = strlen(buffer);
+      if(buffer[pos-1] == '\n') buffer[pos-1] = '\0';
+
+      send(server, buffer, sizeof(buffer), 0);
+      printf("Message sent!\n");
+    }
+
+    closesocket(server);
+    WSACleanup();
+    printf("Socket closed.\n");
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <winsock2.h>
+
+using namespace std;
+
+// -lws2_32
+
+int main() {
+    WSADATA WSAData;
+    SOCKET server, client;
+    SOCKADDR_IN serverAddr, clientAddr;
+
+    WSAStartup(MAKEWORD(2,0), &WSAData);
+    server = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(5555);
+
+    bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+    listen(server, 0);
+
+    printf("Listening for incoming connections...\n");
+
+    char buffer[1024];
+    int clientAddrSize = sizeof(clientAddr);
+
+    client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize);
+
+    if(client != INVALID_SOCKET) {
+        printf("Client connected!\n");
+
+        while(recv(client, buffer, sizeof(buffer), 0) != 0) {
+          printf("Client says: %s\n", buffer);
+          memset(buffer, 0, sizeof(buffer));
+        }
+
+        closesocket(client);
+        printf("Client disconnected.\n");
+    }
+
+    return 0;
+}

532906cf3e66873fc137a329d1fb67cd2a413140
Arthur F. Rezende <arthurfrezende@gmail.com>
2017-12-01 02:07:14 -0200

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,15 +1,11 @@
 #include <cstdio>
 #include <winsock2.h>
 
-#define byte char
-
 using namespace std;
-
-const byte flag = (char)126;
 
 // compilar com -lws2_32
 
-int main() {
+int main(int argc, char* argv[]) {
     WSADATA WSAData;
     SOCKET server;
     SOCKADDR_IN addr;
@@ -17,25 +13,28 @@ int main() {
     WSAStartup(MAKEWORD(2,0), &WSAData);
     server = socket(AF_INET, SOCK_STREAM, 0);
 
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // ip mesma maquina
+    addr.sin_addr.s_addr = inet_addr(argv[1]); // ip mesma maquina "127.0.0.1"
     addr.sin_family = AF_INET;
     addr.sin_port = htons(5555);
 
     connect(server, (SOCKADDR *)&addr, sizeof(addr));
     printf("Connected to server!\n");
 
+    char s_buffer[5];
+    char r_buffer[5];
+    int numPac = 9;
 
     while(true) {
-      printf("Mensagem a ser enviada: ");
-      byte buffer[1024];
-      fgets(buffer, 1024, stdin);
+      if(!(r_buffer[0] == 'R' && r_buffer[0] == 'N' && r_buffer[0] == 'R')) {
+        printf("Mensagem a ser enviada: ");
+        fgets(s_buffer, 5, stdin);
+        send(server, s_buffer, sizeof(s_buffer), 0);
+      }
+      else ;
 
-      // removendo a quebra de linha
-      int pos = strlen(buffer);
-      if(buffer[pos-1] == '\n') buffer[pos-1] = '\0';
 
-      send(server, buffer, sizeof(buffer), 0);
-      printf("Message sent!\n");
+      recv(server, r_buffer, sizeof(r_buffer), 0);
+      printf("Mensagem recebida: %s\n", r_buffer);
     }
 
     closesocket(server);

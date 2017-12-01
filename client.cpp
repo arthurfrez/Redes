1863e5d50488988464e7631d747fc548125031f8
Arthur F. Rezende <arthurfrezende@gmail.com>
2017-12-01 11:57:24 -0200

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,9 +1,16 @@
 #include <cstdio>
 #include <winsock2.h>
+#include <unistd.h>
 
 using namespace std;
 
 // compilar com -lws2_32
+
+bool startsWith(const char *pre, const char *str) {
+    size_t lenpre = strlen(pre),
+           lenstr = strlen(str);
+    return lenstr < lenpre ? false : strncmp(pre, str, lenpre) == 0;
+}
 
 int main(int argc, char* argv[]) {
     WSADATA WSAData;
@@ -22,19 +29,28 @@ int main(int argc, char* argv[]) {
 
     char s_buffer[5];
     char r_buffer[5];
-    int numPac = 9;
 
     while(true) {
-      if(!(r_buffer[0] == 'R' && r_buffer[0] == 'N' && r_buffer[0] == 'R')) {
-        printf("Mensagem a ser enviada: ");
+      if(startsWith("disc", s_buffer)) break;
+
+      if(startsWith("rnr", r_buffer)) {
+        send(server, "rr0p", sizeof(s_buffer), 0);
+      }
+      else if(startsWith("rr", r_buffer) && r_buffer[3] == 'f') {
+        send(server, s_buffer, sizeof(s_buffer), 0);
+      }
+      else if(startsWith("rej", r_buffer)) {
+        s_buffer[1] = r_buffer[3];
+        send(server, s_buffer, sizeof(s_buffer), 0);
+      }
+      else {
+        printf("Send: ");
         fgets(s_buffer, 5, stdin);
         send(server, s_buffer, sizeof(s_buffer), 0);
       }
-      else ;
-
 
       recv(server, r_buffer, sizeof(r_buffer), 0);
-      printf("Mensagem recebida: %s\n", r_buffer);
+      printf("Server: %s\n", r_buffer);
     }
 
     closesocket(server);
